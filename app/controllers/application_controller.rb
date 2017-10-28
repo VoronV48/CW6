b20b154b57489519ad3d62c4bfa5b28f8417b292
Marat Bekkulov <marat@gmail.com>
2017-10-28 15:34:20 +0600

    add cancan to photos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+    ## to avoid deprecation warnings with Rails 3.2.x (and incidentally using Ruby 1.9.3 hash syntax)
+    ## this render call should be:
+    # render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
+  end
+  
+  private
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,25 +1,31 @@
 class PhotosController < InheritedResources::Base
   def index
-    @photos = Photo.all
+    @photos = Photo.order(created_at: :desc)
   end
 
   def show
+  	authorize! :read, @photo
+  	@photo = Photo.find(params[:id])
   end
 
   def new
+  	authorize! :create, @photo
     @photo = Photo.new
   end
 
   def create
+  	authorize! :create, @photo
   	@photo = current_user.photos.create(photo_params)
     @photo.save
     redirect_to root_path
   end
 
   def destroy
+  	authorize! :destroy, @photo
+  	@photo = Photo.find(params[:id]) 
     @photo.destroy
     respond_to do |format|
-      format.html { redirect_to photos_url, notice: 'pPhoto was successfully destroyed.' }
+      format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
